a402fabfd2702df4cd8b8a4c2fefd042eb25f29a
Richard <Richard>
2019-03-31 23:25:35 +0000

    fixe

--- examples/echo/src/lib.rs
+++ examples/echo/src/lib.rs
@@ -13,16 +13,14 @@ struct CommandLineArguments {
 
 extern "C" {
     fn console_log(msg:CString);
-    fn joss_syscall(request:CString)->CString;
 }
 
 #[no_mangle]
 fn main() {
-    // The type of `john` is `serde_json::Value`
     let request_json = json!({
         "operation": "get_command_line_arguments"
     });
-    let response = joss::syscall(request_json.to_string())
+    let response = joss::syscall(request_json.to_string());
     let response_json:CommandLineArguments = from_str(&cstr_to_string(response)).unwrap();
     unsafe {
         console_log(cstr(&response_json.arguments.clone().join(" ")));
